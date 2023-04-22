d40fb5784b0b57f755f7b02720250e375d66d910
Mohamed Adel <Mohamed.Adel.Mohamed.Ali@ibm.com>
2023-04-22 02:28:52 +0200

    init aws cdk

---
+++ test/learning-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearningAwsCdk from '../lib/learning-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learning-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearningAwsCdk.LearningAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

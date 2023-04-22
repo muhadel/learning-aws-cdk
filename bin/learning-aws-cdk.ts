dc4c8c701221293777462b61bc186f102fa46ffb
Mohamed Adel <Mohamed.Adel.Mohamed.Ali@ibm.com>
2023-04-22 04:00:03 +0200

    removed unneeded files

--- bin/learning-aws-cdk.ts
+++ bin/learning-aws-cdk.ts
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
+import * as dotenv from 'dotenv';
+dotenv.config();
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LearningAwsCdkStack } from '../lib/learning-aws-cdk-stack';
+import { MyNetworkStack } from '../lib/MyNetworkStack';
 
 const app = new cdk.App();
-new LearningAwsCdkStack(app, 'LearningAwsCdkStack', {
+
+new MyNetworkStack(app, 'MyNetworkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // env: { account: process.env.DEFAULT_ACCOUNT, region: process.env.DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
